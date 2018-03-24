16267eab1fd1bbc3e0d880161ed51ba0afa54373
Pedro Feiteira <pedro.feiteira@gmail.com>
2018-03-24 14:47:44 +0100

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBIG98WFlGqrKz-vE7So3pCO_BM2Q0gkhc",
+    authDomain: "angular-chat-pmf.firebaseapp.com",
+    databaseURL: "https://angular-chat-pmf.firebaseio.com",
+    projectId: "angular-chat-pmf",
+    storageBucket: "angular-chat-pmf.appspot.com",
+    messagingSenderId: "385206860198"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBIG98WFlGqrKz-vE7So3pCO_BM2Q0gkhc",
+    authDomain: "angular-chat-pmf.firebaseapp.com",
+    databaseURL: "https://angular-chat-pmf.firebaseio.com",
+    projectId: "angular-chat-pmf",
+    storageBucket: "angular-chat-pmf.appspot.com",
+    messagingSenderId: "385206860198"
+  }
 };
